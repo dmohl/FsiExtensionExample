2a7e8203c08b72542aa2045622ab51b50258c204
Dan Mohl <dmohl@yahoo.com>
2011-08-25 21:47:07 -0500

    updated the script

--- TestFsDte/FsiExtension.fsx
+++ TestFsDte/FsiExtension.fsx
@@ -5,71 +5,73 @@
 #r "System.Xml.Linq"
 #r @"C:\Program Files (x86)\Microsoft ASP.NET\ASP.NET Web Pages\v1.0\Assemblies\NuGet.Core.dll"
 
-open System.IO
-open EnvDTE
-open EnvDTE80
-open System.Runtime.InteropServices
-open NuGet
+module internal nuget = 
+    open System.IO
+    open EnvDTE
+    open EnvDTE80
+    open System.Runtime.InteropServices
+    open NuGet
 
-let packageSourceUrl = "https://go.microsoft.com/fwlink/?LinkID=206669"
-let packageSource = PackageSource(packageSourceUrl)
-let repositoryFactory = PackageRepositoryFactory() 
-let repository = repositoryFactory.CreateRepository (packageSource)
+    let packageSourceUrl = "https://go.microsoft.com/fwlink/?LinkID=206669"
+    let packageSource = PackageSource(packageSourceUrl)
+    let repositoryFactory = PackageRepositoryFactory() 
+    let repository = repositoryFactory.CreateRepository packageSource
 
-let automationObject = Marshal.GetActiveObject("VisualStudio.DTE.10.0")
-let dte2 = automationObject :?> DTE2
+    let automationObject = Marshal.GetActiveObject("VisualStudio.DTE.10.0")
+    let dte2 = automationObject :?> DTE2
 
-let documentPath = match dte2.Solution with
-            | null -> dte2.ActiveDocument.Path
-            | _ -> Path.GetDirectoryName dte2.Solution.FullName
+    let documentPath = match dte2.Solution with
+                       | null -> dte2.ActiveDocument.Path
+                       | _ -> Path.GetDirectoryName dte2.Solution.FullName
 
-let SendToFsiConsole text = 
-    printfn "%s\r\n" text
+    let SendToFsiConsole text = printfn "%s\r\n" text
 
-let GetRepositoryPath path = 
-    Path.Combine(path, "packages")
+    let GetRepositoryPath path = Path.Combine(path, "packages")
 
-let InstallPackages (repositoryId:string) (destinationPath:string) =
-    "Creating the package manager." |> SendToFsiConsole
-    let packageManager = PackageManager(repository, DefaultPackagePathResolver documentPath, PhysicalFileSystem(GetRepositoryPath destinationPath))
-    "Retrieving the package from the NuGet Repository. This may take several minutes..." |> SendToFsiConsole
-    packageManager.InstallPackage repositoryId
+    let InstallPackages (repositoryId:string) (destinationPath:string) =
+        "Creating the package manager." |> SendToFsiConsole
+        let packageManager = PackageManager(repository, DefaultPackagePathResolver documentPath, 
+                                 PhysicalFileSystem(GetRepositoryPath destinationPath))
+        "Retrieving the package from the NuGet Repository. This may take several minutes..." |> SendToFsiConsole
+        packageManager.InstallPackage repositoryId
     
-let AddReferenceToFsx referenceName =
-    if dte2.ActiveDocument.Name.Contains ".fsx" then
-        let doc = dte2.ActiveDocument.Object "TextDocument" :?> TextDocument
-        let editPoint = doc.CreateEditPoint()
-        if not ((editPoint.GetText doc.EndPoint).Contains(referenceName)) then
-            editPoint.LineDown 1
-            editPoint.Insert(sprintf "#r @\"%s\"\r\n" referenceName)
+    let AddReferenceToFsx referenceName =
+        if dte2.ActiveDocument.Name.Contains ".fsx" then
+            let doc = dte2.ActiveDocument.Object "TextDocument" :?> TextDocument
+            let editPoint = doc.CreateEditPoint()
+            if not ((editPoint.GetText doc.EndPoint).Contains(referenceName)) then
+                editPoint.Insert(sprintf "#r @\"%s\"\r\n" referenceName)
 
-let (|IsFrameworkSpecific|_|) (input:string) =
-    let libIndex = input.IndexOf("\\lib\\") + 5
-    let modifiedInput = input.Substring(libIndex, input.Length - libIndex)
-    match modifiedInput.Contains("\\") with
-    | true -> Some ()
-    | _ -> None
+    let (|IsFrameworkSpecific|_|) (input:string) =
+        let libIndex = input.IndexOf("\\lib\\") + 5
+        let modifiedInput = input.Substring(libIndex, input.Length - libIndex)
+        match modifiedInput.Contains("\\") with
+        | true -> Some ()
+        | _ -> None
 
-let UpdateReference path =    
-    Directory.GetFiles(GetRepositoryPath path, "*.dll", SearchOption.AllDirectories) 
-    |> Seq.filter(fun file -> file.ToLower().Contains("lib") 
-                              && (not (file.ToLower().Contains("tools")))
-                              && (not (file.ToLower().Contains("content"))) )
-    |> Seq.iter(fun file -> 
-                    match file with
-                    | IsFrameworkSpecific -> 
-                        // TODO: Need to make this better
-                        if file.ToUpper().Contains("NET40") then
-                            AddReferenceToFsx file
-                    | _ -> AddReferenceToFsx file )
+    let UpdateReference path =    
+        Directory.GetFiles(GetRepositoryPath path, "*.dll", SearchOption.AllDirectories) 
+        |> Seq.filter(fun file -> file.ToLower().Contains("lib") 
+                                  && (not (file.ToLower().Contains("tools")))
+                                  && (not (file.ToLower().Contains("content"))) )
+        |> Seq.iter(fun file -> 
+                        match file with
+                        | IsFrameworkSpecific -> 
+                            if file.ToLower().Contains("net40") then
+                                AddReferenceToFsx file
+                        | _ -> AddReferenceToFsx file )
 
-let InstallPackage packageName = 
-    InstallPackages packageName documentPath
-    sprintf "%s has been retrieved successfully. Now updating the references." packageName |> SendToFsiConsole
-    UpdateReference documentPath
-    sprintf "%s has been added successfully." packageName |> SendToFsiConsole 
+    let InstallPackage packageName = 
+        InstallPackages packageName documentPath
+        sprintf "%s has been retrieved successfully. Now updating the references." packageName |> SendToFsiConsole
+        UpdateReference documentPath
+        sprintf "%s has been added successfully." packageName |> SendToFsiConsole 
  
-let ShowInstalledPackages () =
-    Directory.GetDirectories(GetRepositoryPath documentPath)
-    |> Seq.iter (fun dir -> let array = dir.Split([|'\\'|]) |> Array.rev  
-                            array.[0] |> SendToFsiConsole )
+    let ShowInstalledPackages () =
+        Directory.GetDirectories(GetRepositoryPath documentPath)
+        |> Seq.iter (fun dir -> let array = dir.Split([|'\\'|]) |> Array.rev 
+                                array.[0] |> SendToFsiConsole )
+
+let InstallPackage packageName = nuget.InstallPackage packageName
+ 
+let ShowInstalledPackages () = nuget.ShowInstalledPackages()
---
+++ TestFsDte/TestScript.fsx
@@ -0,0 +1,16 @@
+﻿open Microsoft.FSharp.Control  
+  
+let rec job = async {     
+        for i in 1 .. 20 do    
+            printfn "doing some work"  
+            do! Async.Sleep 300  
+            worker.ReportProgress i  
+    }  
+    and worker : AsyncWorker<_> = AsyncWorker(job)  
+  
+worker.ProgressChanged.Add(fun jobNumber -> printfn "job %d completed" jobNumber)   
+worker.Error.Add(fun err -> printfn "Error: %A" err.Message)  
+worker.Completed.Add(fun _ -> printfn "All jobs have completed")  
+worker.Canceled.Add(fun _ -> printfn "Jobs have been canceled")  
+  
+worker.RunAsync() |> ignore 
