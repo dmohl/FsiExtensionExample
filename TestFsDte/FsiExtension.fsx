9b35084feec0c94c1dd324ee97164a538c2ab0ca
Dan Mohl <dmohl@yahoo.com>
2011-08-27 09:25:56 -0500

    A few bug fixes

--- TestFsDte/FsiExtension.fsx
+++ TestFsDte/FsiExtension.fsx
@@ -17,12 +17,16 @@ module internal nuget =
     let repositoryFactory = PackageRepositoryFactory() 
     let repository = repositoryFactory.CreateRepository packageSource
 
-    let automationObject = Marshal.GetActiveObject("VisualStudio.DTE.10.0")
-    let dte2 = automationObject :?> DTE2
+    let GetDte2() =
+        Marshal.GetActiveObject("VisualStudio.DTE.10.0") :?> DTE2
 
-    let documentPath = match dte2.Solution with
-                       | null -> dte2.ActiveDocument.Path
-                       | _ -> Path.GetDirectoryName dte2.Solution.FullName
+    let GetDocumentPath () = 
+        try      
+            match GetDte2() with
+            | dte2 when dte2.Solution = null -> dte2.ActiveDocument.Path
+            | dte2 -> Path.GetDirectoryName dte2.Solution.FullName
+        with
+        | _ -> failwith "A solution must be loaded."
 
     let SendToFsiConsole text = printfn "%s\r\n" text
 
@@ -30,12 +34,13 @@ module internal nuget =
 
     let InstallPackages (repositoryId:string) (destinationPath:string) =
         "Creating the package manager." |> SendToFsiConsole
-        let packageManager = PackageManager(repository, DefaultPackagePathResolver documentPath, 
+        let packageManager = PackageManager(repository, DefaultPackagePathResolver destinationPath, 
                                  PhysicalFileSystem(GetRepositoryPath destinationPath))
         "Retrieving the package from the NuGet Repository. This may take several minutes..." |> SendToFsiConsole
         packageManager.InstallPackage repositoryId
     
     let AddReferenceToFsx referenceName =
+        let dte2 = GetDte2()
         if dte2.ActiveDocument.Name.Contains ".fsx" then
             let doc = dte2.ActiveDocument.Object "TextDocument" :?> TextDocument
             let editPoint = doc.CreateEditPoint()
@@ -61,16 +66,21 @@ module internal nuget =
                                 AddReferenceToFsx file
                         | _ -> AddReferenceToFsx file )
 
-    let InstallPackage packageName = 
+    let InstallPackage packageName =
+        let documentPath = GetDocumentPath() 
         InstallPackages packageName documentPath
         sprintf "%s has been retrieved successfully. Now updating the references." packageName |> SendToFsiConsole
         UpdateReference documentPath
         sprintf "%s has been added successfully." packageName |> SendToFsiConsole 
  
     let ShowInstalledPackages () =
-        Directory.GetDirectories(GetRepositoryPath documentPath)
-        |> Seq.iter (fun dir -> let array = dir.Split([|'\\'|]) |> Array.rev 
-                                array.[0] |> SendToFsiConsole )
+        try
+            let documentPath = GetDocumentPath()            
+            Directory.GetDirectories(GetRepositoryPath documentPath)
+            |> Seq.iter (fun dir -> let array = dir.Split([|'\\'|]) |> Array.rev 
+                                    array.[0] |> SendToFsiConsole )
+        with
+        | _ -> "There are no packages currently installed" |> SendToFsiConsole 
 
 let InstallPackage packageName = nuget.InstallPackage packageName
  
